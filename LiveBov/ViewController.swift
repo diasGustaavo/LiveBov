cf5afff7fa8113fb33614e0f27b277beb9b1927e
diasGustaavo <diasgustaavo@icloud.com>
2022-12-16 16:36:10 -0300

    Initial Commit

---
+++ LiveBov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveBov
+//
+//  Created by Gustavo Dias on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
